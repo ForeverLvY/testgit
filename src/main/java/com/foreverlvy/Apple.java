9c78cddf706a280cd641ee2cffdf3a0dfd01545e
ForeverLvY <ForeverLvY@163.com>
2019-05-11 14:29:52 +0800

    提交分支代码

--- src/main/java/com/foreverlvy/Apple.java
+++ src/main/java/com/foreverlvy/Apple.java
@@ -4,4 +4,7 @@ package com.foreverlvy;
  * Created by ForeverLvY on 2019/5/9.
  */
 public class Apple {
+    public static void main(String[] args) {
+        System.out.println("新建了一个无用分支");
+    }
 }
