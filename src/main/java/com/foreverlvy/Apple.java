b4eab72fcb21743df64fb7688c59e975f9383992
ForeverLvY <ForeverLvY@163.com>
2019-05-11 15:02:17 +0800

    2搜索

--- src/main/java/com/foreverlvy/Apple.java
+++ src/main/java/com/foreverlvy/Apple.java
@@ -5,6 +5,6 @@ package com.foreverlvy;
  */
 public class Apple {
     public static void main(String[] args) {
-        System.out.println("新建了一个无用分支，大爷先提交了222");
+        System.out.println("新建了一个无用分支，大爷先提交了2222222222222222");
     }
 }
